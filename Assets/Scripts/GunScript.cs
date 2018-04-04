3b0afb63f5fe113a6bf59a612a8e152209c7532a
Ian MacKenzie <32595467+Shoebopadoo@users.noreply.github.com>
2018-04-04 01:29:04 -0400

    First Night update
    I hate my self for being slow at development

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour {
+
+    public GameObject bullet = null;    //Projectile that the gun fires
+    public Transform muzzlePoint;       //End of the barrel of the gun
+
+    private int LastFrameSHot = -1;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            FireProjectile();
+        }
+		
+	}
+
+    void FireProjectile()
+    {
+        GameObject newBullet = Instantiate(bullet, muzzlePoint.position, muzzlePoint.rotation);
+        
+    }
+}
