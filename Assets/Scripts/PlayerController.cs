8864cfb331c49db9c3de0f236207582a4631f8c4
NHTI <nhti@conc.students.local>
2018-04-09 17:48:05 -0400

    Added PlayerController
    Now working on the player controller, I am testing with a block with a dinky little gun and sheild

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    GameObject weapon = null;
+    GameObject shield = null;
+
+    int maxBullets = -1;
+
+    List<GameObject> blist = new List<GameObject>(); //Holds a list of all bullets for that player
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
