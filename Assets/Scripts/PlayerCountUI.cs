1668cd75162be15e809439e46e0d1ecc4661b522
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-04-10 12:40:12 -0400

    setting up player count ui

---
+++ Assets/Scripts/PlayerCountUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerCountUI : Photon.PunBehaviour {
+
+    public Text countText;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(PhotonNetwork.countOfPlayers == 1)
+        {
+            countText.text = "1/2" + Environment.NewLine + "Players";
+        }
+        else if(PhotonNetwork.countOfPlayers == 2)
+        {
+            countText.text = "2/2" + Environment.NewLine + "Players";
+        }
+	}
+}
