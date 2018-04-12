f1cf5139b0f939dddae001009629d1548d0311ad
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-04-11 21:51:34 -0400

    plz werk

--- Assets/Scripts/Gameplay/GameManager.cs
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -2,50 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LobbyManager : Photon.PunBehaviour
+public class GameManager : Photon.PunBehaviour
 {
+    public GameObject player;
     public GameObject MasterSpawn;
     public GameObject Spawn;
-    public GameObject player;
-    public bool masterStart = false;
-    public bool playerStart = false;
 
-  
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
-        
-
-        //MasterSpawn = GameObject.Find("SpawnMaster");
-        //Spawn = GameObject.Find("SpawnMaster");
-
+       
         PhotonNetwork.JoinRandomRoom();
-        
-    
-        
     }
 	
 	// Update is called once per frame
-	void Update ()
-    {
-        if(masterStart == true && playerStart == true)
-        {
-            if (PhotonNetwork.isMasterClient)
-            {
-                PhotonNetwork.LoadLevel(2);
-            }
-        }	
-        
-
-	}
-
-    void Awake()
+	void Awake ()
     {
         PhotonNetwork.automaticallySyncScene = true;
-        if (PhotonNetwork.isMasterClient)
-        {
-            Debug.Log("MEE");
-        }
     }
 
     public override void OnPhotonRandomJoinFailed(object[] codeAndMsg)
---
+++ Assets/Scripts/Networking/NetworkedPlayerController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace Riot
+{
+    public class NetworkedPlayerController : Photon.MonoBehaviour
+    {
+
+        [SerializeField]
+        GameObject weapon = null;
+
+        [SerializeField]
+        GameObject shield = null;
+
+        [SerializeField]
+        GameObject bullet = null;
+
+
+        int bulletsLeft = -1;
+        int bulletsFired = -1;
+        public int maxBullets = -1;
+        public int damage = -1;
+
+
+        public int playerID = 1;
+
+        bool isShooting = false;
+
+        public float rateOfFire = -1.0f;
+        float nextFire = 1.0f;
+
+
+
+        List<GameObject> blist = new List<GameObject>(); //Holds a list of all bullets for that player
+                                                         // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            updateControllers();
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+
+            if (Input.GetKeyDown(KeyCode.Mouse0) || (Input.GetButtonDown("Fire1")) || Input.GetButtonDown("RSelectTrigger"))
+            {
+                FireWeapon();
+            }
+        }
+
+        void FireWeapon()
+        {
+            if (Time.time < nextFire)
+            {
+                Debug.Log("Whoa there Partner, its not like the bullets are teleporting into the chamber");
+                return;
+            }
+            else if (bulletsLeft == 0)
+            {
+                Debug.Log("You have already shot all your bullets, Catch some more with your shield");
+                return;
+            }
+            isShooting = true;
+
+            nextFire = Time.time + rateOfFire;
+            GameObject newBullet = Instantiate(bullet, weapon.transform.position + weapon.transform.forward, weapon.transform.rotation);
+            newBullet.GetComponent<NormalBullet>().FireBullet(weapon, damage, playerID);
+            blist.Add(newBullet);
+            bulletsFired++;
+            bulletsLeft = maxBullets - blist.Count;
+        }
+
+        public void OnChildCollisionEnter(Collision collision)
+        {
+            if (collision.transform.tag == "Bullet")
+            {
+                if (blist.Contains(collision.gameObject))
+                {
+                    blist.Remove(collision.gameObject);
+                    bulletsLeft++;
+                    Destroy(collision.gameObject);
+
+                }
+                else
+                {
+                    maxBullets++;
+                    Destroy(collision.gameObject);
+                }
+            }
+        }
+
+        public void updateControllers()
+        {
+            //weapon.transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+            //weapon.transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+
+            //shield.transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+            //shield.transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.LTouch);
+        }
+    }
+}
