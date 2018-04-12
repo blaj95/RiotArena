b6b47115327a06ae09f697b027fe8b4940ad02f3
Ian MacKenzie <32595467+Shoebopadoo@users.noreply.github.com>
2018-04-11 20:17:55 -0400

    The CUBE!

---
+++ Assets/Scripts/Other/CubeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+    [SerializeField]
+    GameObject RealWalls;
+    [SerializeField]
+    GameObject PlaneWalls;
+
+    private void Awake()
+    {
+        RealWalls.SetActive(true);
+        PlaneWalls.SetActive(false);
+    }
+}
