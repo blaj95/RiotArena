c32ad7f7efe569bbd3acf761451f9bd00fab15ef
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-04-13 13:08:17 -0400

    maybe this will work

--- Assets/Scripts/Gameplay/BulletNeworked.cs
+++ Assets/Scripts/Gameplay/BulletNeworked.cs
@@ -2,41 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BulletNeworked : Photon.MonoBehaviour {
-
-    private Vector3 correctBulletPos;
-    private Vector3 updatedBulletPos;
-
-    private Vector3 correctRgbVel;
-    private Vector3 updatedRbgVel;
-
-    private Vector3 correctRgbAng;
-    private Vector3 updatedRbgAng;
-
-    private Vector3 correctRgbPos;
-    private Vector3 updatedRgbPos;
-
-    private Quaternion correctBulletRot; 
-    private Quaternion updatedBulletRot;
-
-    private Quaternion correctRgbRot;
-    private Quaternion updatedRgbRot;
-
-    private float lastSynchronizationTime = 0f;
-    private float syncDelay = 0f;
-    private float syncTime = 0f;
-    private Vector3 syncStartPosition = Vector3.zero;
-    private Vector3 syncEndPosition = Vector3.zero;
-
+public class BulletNeworked : Photon.MonoBehaviour
+{
 
     Rigidbody rigidB;
 
     private float fraction;
 
     public float bulletSpeedIncrease;
-    public float bulletSpeed = 1000f;
+    public float bulletSpeed = 10f;
+
     private Vector3 vel = new Vector3();
-    Vector3 realVelocity2 = Vector3.zero;
+   
 
     bool fired = false;
     int _damage = 1;
@@ -45,24 +22,6 @@ public class BulletNeworked : Photon.MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-        correctBulletPos = transform.localPosition;
-        updatedBulletPos = transform.localPosition;
-
-        correctBulletRot = transform.localRotation;
-        updatedBulletRot = transform.localRotation;
-
-        correctRgbPos = rigidB.position;
-        updatedRgbPos = rigidB.position;
-
-        correctRgbRot = rigidB.rotation;
-        updatedRgbRot = rigidB.rotation;
-
-        correctRgbVel = rigidB.velocity;
-        updatedRbgVel = rigidB.velocity;
-
-        correctRgbAng = rigidB.angularVelocity;
-        updatedRbgAng = rigidB.angularVelocity;
-
         vel = transform.forward;  
     }
 
@@ -72,106 +31,14 @@ public class BulletNeworked : Photon.MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update ()
-    {
-        if (!photonView.isMine)
-        {
-            syncTime += Time.deltaTime;
-            //Update remote player 
-            transform.localPosition = Vector3.Lerp(syncStartPosition, syncEndPosition, (syncTime / syncDelay));
-            transform.localRotation = Quaternion.Lerp(updatedBulletRot, correctBulletRot, (syncTime / syncDelay));
-        }
-   
-    }
+  
 
     private void FixedUpdate()
     {
         if (fired)
         {
-
             //transform.position += transform.forward * (bulletSpeed * Time.deltaTime);
             gameObject.GetComponent<Rigidbody>().MovePosition(transform.localPosition + vel * bulletSpeed * Time.deltaTime);
-        }
-
-        fraction = fraction + Time.deltaTime * 10;
-        if (!photonView.isMine)
-        {
-            syncTime += Time.deltaTime;
-            //Update remote player 
-        
-            rigidB.velocity = Vector3.Lerp(updatedRbgVel, correctRgbVel, (syncTime / syncDelay));
-
-            rigidB.angularVelocity = Vector3.Lerp(updatedRbgAng, correctRgbAng, (syncTime / syncDelay));
-        }
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.isWriting)
-        {
-            Vector3 pos = transform.localPosition;
-            Quaternion rot = transform.localRotation;
-            Vector3 rigPos = rigidB.position;
-            Quaternion rigRot = rigidB.rotation;
-            Vector3 rigVel = rigidB.velocity;
-            Vector3 rigAng = rigidB.angularVelocity;
-
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-            stream.SendNext(rigidB.position);
-            stream.SendNext(rigidB.rotation);
-            stream.SendNext(rigidB.velocity);
-            stream.SendNext(rigidB.angularVelocity);
-
-            stream.Serialize(ref pos);
-            stream.Serialize(ref rot);
-            stream.Serialize(ref rigPos);
-            stream.Serialize(ref rigRot);
-            stream.Serialize(ref rigVel);
-            stream.Serialize(ref rigAng);
-        }
-        else
-        {
-            correctBulletPos = (Vector3)stream.ReceiveNext();
-            correctBulletRot = (Quaternion)stream.ReceiveNext();
-            correctRgbPos = (Vector3)stream.ReceiveNext();
-            correctRgbRot = (Quaternion)stream.ReceiveNext();
-            correctRgbVel = (Vector3)stream.ReceiveNext();
-            correctRgbAng = (Vector3)stream.ReceiveNext();
-
-            Vector3 pos = Vector3.zero;
-            Quaternion rot = Quaternion.identity;
-            Vector3 rigPos = Vector3.zero;
-            Quaternion rigRot = Quaternion.identity;
-            Vector3 rigVel = Vector3.zero;
-            Vector3 rigAng = Vector3.zero;
-
-
-            stream.Serialize(ref pos);
-            stream.Serialize(ref rot);
-            stream.Serialize(ref rigPos);
-            stream.Serialize(ref rigRot);
-            stream.Serialize(ref rigVel);
-            stream.Serialize(ref rigAng);
-
-
-            updatedBulletPos = transform.localPosition;
-            updatedBulletRot = transform.localRotation;
-            updatedRbgVel = rigidB.velocity;
-            updatedRbgAng = rigidB.angularVelocity;
-            updatedRgbPos = rigidB.position;
-            updatedRgbRot = rigidB.rotation;
-
-            syncTime = 0f;
-            lastSynchronizationTime = Time.time;
-            syncDelay = Time.time - lastSynchronizationTime;
-
-
-            syncEndPosition = correctBulletPos + correctRgbVel * syncDelay;
-            syncStartPosition = rigidB.position;
-
-            fraction = 0;
-
         }
     }
 
