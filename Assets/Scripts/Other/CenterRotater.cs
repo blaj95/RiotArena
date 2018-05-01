bdd07103ae7b313ed22056ca4668a73d7d2c8460
Ian MacKenzie <32595467+Shoebopadoo@users.noreply.github.com>
2018-04-30 20:08:55 -0400

    ARENA PUSH
    It looks soo GOOOODD :D

---
+++ Assets/Scripts/Other/CenterRotater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterRotater : MonoBehaviour {
+
+    private Vector3 newRot = new Vector3();
+    IEnumerator timer;
+	// Use this for initialization
+	void Start ()
+    {
+        timer = drainHealth();
+        StartCoroutine(timer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(newRot);
+    }
+
+    public IEnumerator drainHealth()
+    {
+        while (true)
+        {
+            newRot =  new Vector3(Random.value, Random.value, Random.value);
+          
+            yield return new WaitForSeconds(3.0f);
+        }
+    }
+
+}
