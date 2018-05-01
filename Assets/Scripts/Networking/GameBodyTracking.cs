601afe331f26d0cf14455a4c4932c1051cad09be
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-04-30 23:56:42 -0400

    changed body tracking

--- Assets/Scripts/Networking/GameBodyTracking.cs
+++ Assets/Scripts/Networking/GameBodyTracking.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameBodyTracking : Photon.MonoBehaviour {
 
+    public GameObject player;
+    public GameObject netPlayer;
+    public GameObject rightHand;
+    private Vector3 correctRightPos;//We lerp towards this
+    private Vector3 onUpdateRightPos;
+    private Quaternion correctRightRot = Quaternion.identity; //We lerp towards this
+    private Quaternion onUpdateRightRot;
+    private float fraction;
+
     public GameObject myhead;
     public Vector3 bodyOffset;
     // Use this for initialization
@@ -23,9 +32,39 @@ public class GameBodyTracking : Photon.MonoBehaviour {
         }
         else
         {
-            myhead = GameObject.Find("GameHead(Clone)");
-
-            transform.position = myhead.transform.position + bodyOffset;
+            transform.localPosition = Vector3.Lerp(onUpdateRightPos, correctRightPos, fraction);
+            transform.localRotation = Quaternion.Lerp(onUpdateRightRot, correctRightRot, fraction);
+            
         }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            Vector3 pos = transform.localPosition;
+           // Quaternion rot = transform.localRotation;
+            stream.SendNext(transform.position);
+           // stream.SendNext(transform.rotation);
+            stream.Serialize(ref pos);
+           // stream.Serialize(ref rot);
+
+        }
+        else
+        {
+            Vector3 pos = Vector3.zero;
+            Quaternion rot = Quaternion.identity;
+
+            stream.Serialize(ref pos);
+           // stream.Serialize(ref rot);
+
+            correctRightPos = pos;
+           // correctRightRot = rot;
+            onUpdateRightPos = transform.localPosition;
+           // onUpdateRightRot = transform.localRotation;
+            fraction = 0;
+
+        }
+
     }
 }
