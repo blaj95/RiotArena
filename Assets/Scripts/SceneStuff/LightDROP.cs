76ad28c2d3cea9ce020343a43475a97197bb6ca5
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-05-03 16:48:13 -0400

    so much good stuff

---
+++ Assets/Scripts/SceneStuff/LightDROP.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightDROP : MonoBehaviour {
+
+    public Light thisLight;
+    public float Speed = 1;
+    public Material rend;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rend.SetColor("_Color", HSBColor.ToColor(new HSBColor(Mathf.PingPong(Time.time * Speed, 1), 1, 1)));
+        thisLight.color = rend.color;
+    }
+}
---
+++ Assets/Scripts/SceneStuff/LightLerp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightLerp : MonoBehaviour {
+
+
+    public float duration = 1.0F;
+    public Color color0 = Color.red;
+    public Color color1 = Color.blue;
+    public Light thisLight;
+    // Use this for initialization
+
+
+
+
+    void Start ()
+    {
+        
+    
+    }
+
+    
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float t = Mathf.PingPong(Time.time, duration) / duration;
+        thisLight.color = Color.Lerp(color0, color1, t);
+
+    }
+}
