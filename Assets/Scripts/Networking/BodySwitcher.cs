ab1a3bb4e807da007d2e44cdb38a4b1568fdd896
blaj95 <32546217+blaj95@users.noreply.github.com>
2018-05-03 21:04:09 -0400

    body switchingg
    and head

---
+++ Assets/Scripts/Networking/BodySwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodySwitcher : MonoBehaviour {
+
+    public GameObject bBody;
+    public GameObject rBody;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            bBody.SetActive(true);
+            rBody.SetActive(false);
+        }
+        else
+        {
+            bBody.SetActive(false);
+            rBody.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Networking/HeadSwitcher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadSwitcher : Photon.MonoBehaviour {
+
+    public GameObject bHead;
+    public GameObject rHead;
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            bHead.SetActive(true);
+            rHead.SetActive(false);
+        }
+        else
+        {
+            bHead.SetActive(false);
+            rHead.SetActive(true);
+        }
+	}
+}
